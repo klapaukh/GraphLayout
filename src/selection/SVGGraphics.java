43963d16085c508c59ce0215a3bf61e31abd1aaf
Roma Klapaukh <rklapaukh@hotmail.com>
2012-06-21 20:18:15 +1200

    Populated methods for the SVGGraphics class to write the XML

--- src/selection/SVGGraphics.java
+++ src/selection/SVGGraphics.java
@@ -27,9 +27,6 @@ import java.util.Map;
 
 import javax.swing.JFrame;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-
 public class SVGGraphics extends Graphics2D {
 	private double alpha = 1;
 	public JFrame frame;
@@ -53,229 +50,241 @@ public class SVGGraphics extends Graphics2D {
 		s.append(height);
 		s.append("\"\n");
 		s.append("zoomAndPan=\"disable\" >\n");
-		this.frame=frame;
+		this.frame = frame;
 	}
 
 	@Override
 	public void draw(Shape s) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public boolean drawImage(Image img, AffineTransform xform, ImageObserver obs) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void drawImage(BufferedImage img, BufferedImageOp op, int x, int y) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void drawRenderedImage(RenderedImage img, AffineTransform xform) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void drawRenderableImage(RenderableImage img, AffineTransform xform) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void drawString(String str, int x, int y) {
-		// TODO Auto-generated method stub
-
+		s.append("<text ");
+		s.append("x=\"");
+		s.append(x);
+		s.append("\" y=\"");
+		s.append(y);
+		s.append("\" color=\"");
+		s.append(color);
+		s.append("\" opacity=\"");
+		s.append(String.format("%.2f", this.alpha));
+		s.append("\">");
+		s.append(str);
+		s.append("</text>");
 	}
 
 	@Override
 	public void drawString(String str, float x, float y) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void drawString(AttributedCharacterIterator iterator, int x, int y) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
-	public void drawString(AttributedCharacterIterator iterator, float x, float y) {
-		throw new NotImplementedException();
+	public void drawString(AttributedCharacterIterator iterator, float x,
+			float y) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void drawGlyphVector(GlyphVector g, float x, float y) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void fill(Shape s) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public boolean hit(Rectangle rect, Shape s, boolean onStroke) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public GraphicsConfiguration getDeviceConfiguration() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setComposite(Composite comp) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setPaint(Paint paint) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setStroke(Stroke s) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setRenderingHint(Key hintKey, Object hintValue) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Object getRenderingHint(Key hintKey) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setRenderingHints(Map<?, ?> hints) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void addRenderingHints(Map<?, ?> hints) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public RenderingHints getRenderingHints() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void translate(int x, int y) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void translate(double tx, double ty) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void rotate(double theta) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void rotate(double theta, double x, double y) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void scale(double sx, double sy) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void shear(double shx, double shy) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void transform(AffineTransform Tx) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setTransform(AffineTransform Tx) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public AffineTransform getTransform() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Paint getPaint() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Composite getComposite() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setBackground(Color color) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Color getBackground() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Stroke getStroke() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void clip(Shape s) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public FontRenderContext getFontRenderContext() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Graphics create() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Color getColor() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setColor(Color c) {
-		this.color = "rgb(" + c.getRed() + "," + c.getGreen() + "," + c.getBlue() + ")";
-		this.alpha = c.getAlpha()/255.0;
+		this.color = "rgb(" + c.getRed() + "," + c.getGreen() + ","
+				+ c.getBlue() + ")";
+		this.alpha = c.getAlpha() / 255.0;
 	}
 
 	@Override
 	public void setPaintMode() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setXORMode(Color c1) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Font getFont() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setFont(Font font) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
@@ -285,32 +294,32 @@ public class SVGGraphics extends Graphics2D {
 
 	@Override
 	public Rectangle getClipBounds() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void clipRect(int x, int y, int width, int height) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setClip(int x, int y, int width, int height) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public Shape getClip() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void setClip(Shape clip) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void copyArea(int x, int y, int width, int height, int dx, int dy) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
@@ -320,116 +329,189 @@ public class SVGGraphics extends Graphics2D {
 		s.append(x1);
 		s.append("\" y1=\"");
 		s.append(y1);
-		s.append("\" xx2=\"");
+		s.append("\" x2=\"");
 		s.append(x2);
 		s.append("\" y2=\"");
 		s.append(y2);
 		s.append("\" stroke=\"");
 		s.append(color);
-		s.append("\" />\n");
+		s.append("\" opacity=\"");
+		s.append(String.format("%.2f", alpha));
+		s.append("\"/>\n");
 
 	}
 
 	@Override
 	public void fillRect(int x, int y, int width, int height) {
-		// TODO Auto-generated method stub
+		s.append("<rect ");
+		s.append("x=\"");
+		s.append(x);
+		s.append("\" y=\"");
+		s.append(y);
+		s.append("\" width=\"");
+		s.append(width);
+		s.append("\" height=\"");
+		s.append(height);
+		s.append("\" fill=\"");
+		s.append(color);
+		s.append("\" opacity=\"");
+		s.append(String.format("%.2f", alpha));
+		s.append("\"/>\n");
 
 	}
 
 	@Override
 	public void clearRect(int x, int y, int width, int height) {
-		// TODO Auto-generated method stub
-
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
-	public void drawRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
-		// TODO Auto-generated method stub
-
+	public void drawRoundRect(int x, int y, int width, int height,
+			int arcWidth, int arcHeight) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
-	public void fillRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
-		// TODO Auto-generated method stub
-
+	public void fillRoundRect(int x, int y, int width, int height,
+			int arcWidth, int arcHeight) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void drawOval(int x, int y, int width, int height) {
-		// TODO Auto-generated method stub
+		s.append("<ellipse ");
+		s.append("cx=\"");
+		s.append(x);
+		s.append("\" cy=\"");
+		s.append(y);
+		s.append("\" rx=\"");
+		s.append(width);
+		s.append("\" ry=\"");
+		s.append(height);
+		s.append("\" stroke=\"");
+		s.append(color);
+		s.append("\" opacity=\"");
+		s.append(String.format("%.2f", alpha));
+		s.append("\"/>\n");
 
 	}
 
 	@Override
 	public void fillOval(int x, int y, int width, int height) {
-		// TODO Auto-generated method stub
-
+		s.append("<ellipse ");
+		s.append("cx=\"");
+		s.append(x);
+		s.append("\" cy=\"");
+		s.append(y);
+		s.append("\" rx=\"");
+		s.append(width);
+		s.append("\" ry=\"");
+		s.append(height);
+		s.append("\" fill=\"");
+		s.append(color);
+		s.append("\" opacity=\"");
+		s.append(String.format("%.2f", alpha));
+		s.append("\"/>\n");
 	}
 
 	@Override
-	public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
-		// TODO Auto-generated method stub
-
+	public void drawArc(int x, int y, int width, int height, int startAngle,
+			int arcAngle) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
-	public void fillArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
-		// TODO Auto-generated method stub
-
+	public void fillArc(int x, int y, int width, int height, int startAngle,
+			int arcAngle) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void drawPolyline(int[] xPoints, int[] yPoints, int nPoints) {
-		// TODO Auto-generated method stub
+		throw new RuntimeException("Method Not Yet Implemented");
 
 	}
 
 	@Override
 	public void drawPolygon(int[] xPoints, int[] yPoints, int nPoints) {
-		// TODO Auto-generated method stub
+		s.append("<polygon ");
+		s.append("points=\"");
+		for(int i =0 ; i < nPoints; i++){
+			s.append(xPoints[i]);
+			s.append(',');
+			s.append(yPoints[i]);
+			if(i != nPoints -1 ){
+				s.append(',');
+			}
+		}
+		s.append("\" stroke=\"");
+		s.append(color);
+		s.append("\" opacity=\"");
+		s.append(String.format("%.2f", alpha));
+		s.append("\"/>\n");
 
 	}
 
 	@Override
 	public void fillPolygon(int[] xPoints, int[] yPoints, int nPoints) {
-		// TODO Auto-generated method stub
+		s.append("<polygon ");
+		s.append("points=\"");
+		for(int i =0 ; i < nPoints; i++){
+			s.append(xPoints[i]);
+			s.append(',');
+			s.append(yPoints[i]);
+			if(i != nPoints -1 ){
+				s.append(',');
+			}
+		}
+		s.append("\" fill=\"");
+		s.append(color);
+		s.append("\" opacity=\"");
+		s.append(String.format("%.2f", alpha));
+		s.append("\"/>\n");
 
 	}
 
 	@Override
 	public boolean drawImage(Image img, int x, int y, ImageObserver observer) {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
-	public boolean drawImage(Image img, int x, int y, int width, int height, ImageObserver observer) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean drawImage(Image img, int x, int y, int width, int height,
+			ImageObserver observer) {
+		this.fillRect(x, y, width, height);
+		return true;
 	}
 
 	@Override
-	public boolean drawImage(Image img, int x, int y, Color bgcolor, ImageObserver observer) {
-		throw new NotImplementedException();
+	public boolean drawImage(Image img, int x, int y, Color bgcolor,
+			ImageObserver observer) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
-	public boolean drawImage(Image img, int x, int y, int width, int height, Color bgcolor, ImageObserver observer) {
-		throw new NotImplementedException();
+	public boolean drawImage(Image img, int x, int y, int width, int height,
+			Color bgcolor, ImageObserver observer) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
-	public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, ImageObserver observer) {
-		throw new NotImplementedException();
+	public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2,
+			int sx1, int sy1, int sx2, int sy2, ImageObserver observer) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
-	public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, Color bgcolor, ImageObserver observer) {
-		throw new NotImplementedException();
+	public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2,
+			int sx1, int sy1, int sx2, int sy2, Color bgcolor,
+			ImageObserver observer) {
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 	@Override
 	public void dispose() {
-		throw new NotImplementedException();
+		throw new RuntimeException("Method Not Yet Implemented");
 	}
 
 }
