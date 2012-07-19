155d854124d1efd0710a0a49867ecbf9ea0fa37b
Roma Klapaukh <roma@ecs.vuw.ac.nz>
2012-07-20 11:24:28 +1200

    Added font information

--- src/selection/FinalScreen.java
+++ src/selection/FinalScreen.java
@@ -6,14 +6,19 @@ import java.awt.Graphics;
 
 public class FinalScreen extends MoveComponent {
 
-	public FinalScreen(){}
+	private static final long serialVersionUID = 759336901491281162L;
+	private Font font;
+	
+	public FinalScreen(){
+		this.font = new Font("serif",Font.PLAIN,40);
+	}
 	
 	public void paint(Graphics g){
 		g.setColor(Color.white);
 		g.fillRect(0,0,1920,1080);
 		
 		g.setColor(Color.black);
-		g.setFont(new Font("serif",Font.PLAIN,40));
+		g.setFont(font);
 		g.drawString("Thank you for participating", 800, 1080/2);
 	}
 	@Override
