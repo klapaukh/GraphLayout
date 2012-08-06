efb9970a72fddc8a227a622426b5ffdae2f87695
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2012-08-06 16:19:47 +1200

    Add move lost informatoin tracking to the screens. Don't do anything with it yet.

--- src/selection/MoveComponent.java
+++ src/selection/MoveComponent.java
@@ -2,13 +2,29 @@ package selection;
 
 import javax.swing.JComponent;
 
+import nz.ac.vuw.ecs.moveme.MoveLostListener;
 import nz.ac.vuw.ecs.moveme.UpdateListener;
 
-public abstract class MoveComponent extends JComponent implements UpdateListener{
-
+public abstract class MoveComponent extends JComponent implements UpdateListener, MoveLostListener{
+	
+	
+	protected volatile boolean moveLost;
 	protected String filename;
 	private static final long serialVersionUID = 3728612264508579669L;
+	
+	public MoveComponent(){
+		moveLost = false;
+	}
+	
 	public String toSVG(){
 		throw new RuntimeException("BUG!!");
+	}
+	
+	public void moveLost(){
+		this.moveLost = true;
+	}
+	
+	public void moveRegained(){
+		this.moveLost = false;
 	}
 }
