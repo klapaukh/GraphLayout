7fd9a7387fd42b66d2533cff0ea92eb303102dec
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2012-08-06 16:28:24 +1200

    Add in a standard image to show whne the sphere is not visible

--- src/selection/MoveComponent.java
+++ src/selection/MoveComponent.java
@@ -1,5 +1,10 @@
 package selection;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 import nz.ac.vuw.ecs.moveme.MoveLostListener;
@@ -11,9 +16,15 @@ public abstract class MoveComponent extends JComponent implements UpdateListener
 	protected volatile boolean moveLost;
 	protected String filename;
 	private static final long serialVersionUID = 3728612264508579669L;
+	protected Image moveNotThere;
 	
 	public MoveComponent(){
 		moveLost = false;
+		try {
+			moveNotThere = ImageIO.read(new File("images/moveNotThere.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String toSVG(){
